d349a156c30151f5e9b1445e572478338cfea29e
Pranavi Kondapalli <101938627+pranavik24@users.noreply.github.com>
2022-10-09 13:57:52 -0400

    Create NoTimeToPaint.java

---
+++ src/main/java/edu/competition_practice/NoTimeToPaint.java
@@ -0,0 +1,26 @@
+package competition_practice;
+import java.util.*;
+
+public class NoTimeToPaint {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int numFences = sc.nextInt();
+		int numTestCases = sc.nextInt();
+		
+		char[] fenceColors = sc.next().toCharArray();
+		
+		
+		boolean[] ifPainting = new boolean[numFences];
+		int[] numTimesPainted = new int[numFences]; 
+		
+		char currentLetter; 
+		for(int i = 0; i < numFences; i++) {
+			currentLetter = fenceColors[i];
+			
+		}
+		
+	}
+
+}
